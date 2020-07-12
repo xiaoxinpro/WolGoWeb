bc0a661077e9270935503de01909b765f3c1fe9e
chishin <pro@xxgzs.org>
2020-07-12 09:54:38 +0800

    增加环境变量的方式设置参数

--- src/main.go
+++ src/main.go
@@ -5,14 +5,16 @@ import (
 	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"os"
 	"strconv"
 	"time"
 )
 
 var (
-	WebMode = flag.String("mode", gin.ReleaseMode, "wol web port.")
-	WebPort = flag.Int("port", 9090, "wol web port.")
-	ApiKey = flag.String("key", "false", "wol web api key.")
+	ConfigSource string
+	WebMode string
+	WebPort int
+	ApiKey string
 )
 
 var (
@@ -26,10 +28,41 @@ func MD5(str string) string {
 	return md5str
 }
 
+func getEnvString(name string, value string) string {
+	ret := os.Getenv(name)
+	if ret == "" {
+		return value
+	} else {
+		return ret
+	}
+}
+
+func getEnvInt(name string, value int) int {
+	env := os.Getenv(name)
+	if ret, err := strconv.Atoi(env); env == "" || err != nil {
+		return value
+	} else {
+		return ret
+	}
+}
+
+func init()  {
+	flag.StringVar(&ConfigSource,"c", "default", "config source default or env.")
+	flag.StringVar(&WebMode,"mode", gin.ReleaseMode, "wol web port.")
+	flag.IntVar(&WebPort,"port", 9090, "wol web port.")
+	flag.StringVar(&ApiKey,"key", "false", "wol web api key.")
+}
+
 func main()  {
 	flag.Parse()
 
-	gin.SetMode(*WebMode)
+	if ConfigSource == "env" {
+		WebMode = getEnvString("MODE", WebMode)
+		WebPort = getEnvInt("PORT", WebPort)
+		ApiKey = getEnvString("KEY", ApiKey)
+	}
+
+	gin.SetMode(WebMode)
 
 	r:=gin.Default()
 
@@ -37,9 +70,9 @@ func main()  {
 	r.GET("/index", GetIndex)
 	r.GET("/wol", GetWol)
 
-	fmt.Printf("WolGoWeb Runing [port:%d, key:%s]\n", *WebPort, *ApiKey)
+	fmt.Printf("WolGoWeb Runing [port:%d, key:%s]\n", WebPort, ApiKey)
 
-	r.Run(fmt.Sprintf(":%d", *WebPort))
+	r.Run(fmt.Sprintf(":%d", WebPort))
 }
 
 func GetIndex(c *gin.Context) {
@@ -70,7 +103,7 @@ func VerifyAuth(key string, mac string, vk int64, token string) (int, string) {
 		} else if bakVK, ok := vkBakDict[mac]; ok && bakVK == vk {
 			err = 103
 			message = "Time value repetition."
-		} else if MD5(*ApiKey + mac + fmt.Sprintf("%d", vk)) != token {
+		} else if MD5(ApiKey + mac + fmt.Sprintf("%d", vk)) != token {
 			err = 104
 			message = "No authority token."
 		} else {
@@ -86,7 +119,7 @@ func GetWol(c *gin.Context)  {
 	port:=c.DefaultQuery("port", "9")
 	token:=c.DefaultQuery("token", "")
 	vk, _:=strconv.ParseInt(c.DefaultQuery("time", "0"),10, 64)
-	if errAuth, messageAuth := VerifyAuth(*ApiKey, mac, vk, token); errAuth==0  {
+	if errAuth, messageAuth := VerifyAuth(ApiKey, mac, vk, token); errAuth==0  {
 		err:=Wake(mac,ip,port)
 		if err != nil {
 			c.JSON(200, gin.H{
