4a7f8acc76b8981bc70bed090d6899e1f28800f9
chishin <pro@xxgzs.org>
2022-05-23 17:10:29 +0800

    Docker增加多架构构建

---
+++ docker/buildx.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+BUILD_NAME="buildx-wol-go-web"
+BUILD_PLAT=linux/386,linux/amd64,linux/arm/v7,linux/arm64/v8
+BUILD_PUSH=chishin/wol-go-web
+BUILD_VERS=version-0.0.4
+
+echo "=========== ${BUILD_NAME} ==========="
+
+docker buildx create --name ${BUILD_NAME}
+docker buildx use ${BUILD_NAME}
+docker buildx build -t ${BUILD_PUSH}:${BUILD_VERS} -t ${BUILD_PUSH} --progress=plain --platform=${BUILD_PLAT} . --push
+docker buildx rm ${BUILD_NAME}
+
+echo "Multiarch build Complete ${BUILD_NAME} !!!"
