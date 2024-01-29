e6356a90214044341f273c0a53fa8e88cb63088b
chishin <pro@xxgzs.org>
2024-01-29 12:17:37 +0800

    增加构建docker镜像的Actions

--- docker/buildx.sh
+++ docker/buildx.sh
@@ -2,13 +2,13 @@
 BUILD_NAME="buildx-wol-go-web"
 BUILD_PLAT=linux/386,linux/amd64,linux/arm/v7,linux/arm64/v8
 BUILD_PUSH=chishin/wol-go-web
-BUILD_VERS=version-1.5.52
+BUILD_VERSION=$(cat ../.version)
 
 echo "=========== ${BUILD_NAME} ==========="
 
 docker buildx create --name ${BUILD_NAME}
 docker buildx use ${BUILD_NAME}
-docker buildx build -t ${BUILD_PUSH}:${BUILD_VERS} -t ${BUILD_PUSH} --progress=plain --platform=${BUILD_PLAT} . --push
+docker buildx build -t ${BUILD_PUSH}:version-${BUILD_VERSION} -t ${BUILD_PUSH} --progress=plain --platform=${BUILD_PLAT} . --push
 docker buildx rm ${BUILD_NAME}
 
 echo "Multiarch build Complete ${BUILD_NAME} !!!"
